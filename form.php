6ba5de4f8f0a378cdc55fec9946c3ee43838aee4
jonathan <jonathansusanto2015@gmail.com>
2023-10-08 20:18:15 +0700

    first commit

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Form Mahasiswa</h1>
+    <form action="proses.php" method="get" enctype="multipart/form-data">
+        <input type="text" name="nim" />
+        <input type="text" name="nama" />
+        <input type="text" name="prodi" />
+        <input type="file" name="foto" />
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
